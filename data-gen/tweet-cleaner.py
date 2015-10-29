2cf0db77ca1084408de90b67222b325174fd8446
ddrum001 <ddrum001@gmail.com>
2015-10-29 13:44:03 -0400

    changed to filter for geo-enabled and added example tweets JSON

--- data-gen/tweet-cleaner.py
+++ data-gen/tweet-cleaner.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
 # Import the necessary methods from tweepy library
-from __future__ import unicode_literals
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from datetime import datetime
 from pprint import pprint
-import os
 import json
-
+import os
 
 # loads Twitter credentials from .twitter file that is in the same directory as this script
 file_dir = os.path.dirname(os.path.realpath(__file__)) 
 with open(file_dir + '/.twitter') as twitter_file:  
     twitter_cred = json.load(twitter_file)
 
+# authentication from the credentials file above
 access_token = twitter_cred["access_token"]
 access_token_secret = twitter_cred["access_token_secret"]
 consumer_key = twitter_cred["consumer_key"]
@@ -28,6 +27,7 @@ class StdOutListener(StreamListener):
     def __init__(self, filename):
         self.filename = filename
 
+    # this is the event handler for new data
     def on_data(self, data):
         if not os.path.isfile(self.filename):    # check if file doesn't exist
             f = file(self.filename, 'w')
@@ -35,23 +35,17 @@ class StdOutListener(StreamListener):
         with open(self.filename, 'ab') as f:
             print "writing to {}".format(self.filename)
             f.write(data)
-            f.write("\n")
         f.closed
         
+    # this is the event handler for errors    
     def on_error(self, status):
         print(status)
 
-
 if __name__ == '__main__':
-    l = StdOutListener(file_dir + "/tweets.txt")
+    listener = StdOutListener(file_dir + "/tweets.txt")
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     print "Use CTRL + C to exit at any time.\n"
-    stream = Stream(auth, l)
-    stream.filter(track=[
-        '#BigData', '#Spark', '#ApacheKafka', '#ApacheStorm', '#ApacheSpark', 'Data Engineering', 'Hadoop', 'MapReduce',
-        'Mahout', 'MLlib', 'Logstash', 'RabbitMQ', 'Fluentd', 'AWS', 'Avro', '#Samza', 'HBase', '#Cassandra', 'MongoDB',
-        'Elasticsearch', 'Kibana', 'Neo4j', 'CouchDB', 'Redis', 'Memcached', '#Hive', '#ApacehPig', 'Cascalog', 'Giraph',
-        '#Presto', '#Impala', '#ApacheDrill', 'GraphX', 'GraphLab', '#Redshift', 'Solr', '#Riak', 'Hazelcast'
-        ])
+    stream = Stream(auth, listener)
+    stream.filter(locations=[-180,-90,180,90]) # this is the entire world, any tweet with geo-location enabled
